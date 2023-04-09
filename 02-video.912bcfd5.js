b93e3a0ed3e095c7dd4fc94aa4515d5aa151dbaa
BartekMTB <117111323+BartekMTB@users.noreply.github.com>
2023-04-09 12:48:48 +0000

    Deploying to gh-pages from @ BartekMTB/goit-js-hw-08@4413643ddb7d496347c6847ac26565002d587444 🚀

--- 02-video.912bcfd5.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=new Vimeo.Player("vimeo-player");console.log(JSON.parse(localStorage.getItem("videoplayer-current-time")).seconds),e.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")).seconds).then((function(e){})).catch((function(e){e.name})),e.on("timeupdate",(function(e){console.log(e),localStorage.setItem("videoplayer-current-time",JSON.stringify(e))})),e.getVideoTitle().then((function(e){console.log("title:",e)}))}();
-//# sourceMappingURL=02-video.912bcfd5.js.map
--- 02-video.eab0dc15.js
+++
@@ -1,2 +0,0 @@
-const e=new Vimeo.Player("vimeo-player");console.log(JSON.parse(localStorage.getItem("videoplayer-current-time")).seconds),e.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")).seconds).then((function(e){})).catch((function(e){e.name})),e.on("timeupdate",(function(e){console.log(e),localStorage.setItem("videoplayer-current-time",JSON.stringify(e))})),e.getVideoTitle().then((function(e){console.log("title:",e)}));
-//# sourceMappingURL=02-video.eab0dc15.js.map
